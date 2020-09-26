4baebbcbaf241494c784012d9d856121729789f8
Farinhote <andrefarinhote@gmail.com>
2020-09-26 15:40:28 +0100

    Made code generic

---
+++ data/gender.js
@@ -0,0 +1,13 @@
+const urlParams = new URLSearchParams(window.location.search);
+const FEMALE = 'female';
+const MALE = 'male';
+
+
+const gender = urlParams.get('gender');
+const oppositeGender = gender === FEMALE ? MALE : FEMALE;
+const genderKey = gender === FEMALE ? 'F' : '';
+
+const eyeColour = gender === FEMALE ? '3.54-WF-246' : '3.51-WM-015';
+const age = gender === FEMALE ? '3.54-WF-246' : '3.5-WM-253';
+const attentionCheck = gender === FEMALE ? '3.32-WF-038' : '3.51-WM-015';
+const secondAge = gender === FEMALE ? '3.32-WF-038' : '3.51-WM-015';
