15e744eab4fb26c0d920d9c85ceb4d0e083fe74c
ricardoSA54 <RicardoSA54@gmail.com>
2021-02-12 17:04:32 +0100

    .

--- data/pairsV2.js
+++ data/pairsV2.js
@@ -16,6 +16,7 @@ const retifiedPairs = [
               <img src='../img/transparentFaces/transparent${genderKey}/1.73-WM-201-Trans.png'  class = 'picpair' > 
               <img src='../img/transparentFaces/transparent${genderKey}/1.89-WM-215-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/1.61-WF-002-Trans.png'  class = 'partnerL'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 1,
@@ -26,6 +27,7 @@ const retifiedPairs = [
               <img src='../img/transparentFaces/transparent${genderKey}/1.73-WM-201-Trans.png'  class = 'picpair' > 
               <img src='../img/transparentFaces/transparent${genderKey}/1.89-WM-215-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/1.61-WF-002-Trans.png'  class = 'partnerR'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 0,
@@ -38,6 +40,7 @@ const retifiedPairs = [
               <img src='../img/transparentFaces/transparent${genderKey}/4.66-WM-004-Trans.png'  class = 'picpair' > 
               <img src='../img/transparentFaces/transparent${genderKey}/4.59-WM-029-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/2.33-WF-239-Trans.png'  class = 'partnerL'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 1,
@@ -48,17 +51,20 @@ const retifiedPairs = [
               <img src='../img/transparentFaces/transparent${genderKey}/4.66-WM-004-Trans.png'  class = 'picpair' > 
               <img src='../img/transparentFaces/transparent${genderKey}/4.59-WM-029-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/2.33-WF-239-Trans.png'  class = 'partnerR'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 0,
             "right": 1
         }
-    ],[
+    ],
+    [
         {
             "pair": `<div class='bgImgCenter'>
               <img src='../img/transparentFaces/transparent${genderKey}/4.13-WM-207-Trans.png'  class = 'picpair' > 
               <img src='../img/transparentFaces/transparent${genderKey}/4.12-WM-250-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/3.82-WF-244-Trans.png'  class = 'partnerL'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 1,
@@ -66,20 +72,23 @@ const retifiedPairs = [
         },
         {
             "pair": `<div class='bgImgCenter'>
-              <img src='../img/transparentFaces/transparent${genderKey}/4.66-WM-004-Trans.png'  class = 'picpair' > 
-              <img src='../img/transparentFaces/transparent${genderKey}/4.59-WM-029-Trans.png'  class = 'picpair' ></img>
-              <img src='../img/transparentFaces/transparent${genderOppositeKey}/2.33-WF-239-Trans.png'  class = 'partnerR'>  </img> 
+              <img src='../img/transparentFaces/transparent${genderKey}/4.13-WM-207-Trans.png'  class = 'picpair' > 
+              <img src='../img/transparentFaces/transparent${genderKey}/4.12-WM-250-Trans.png'  class = 'picpair' ></img>
+              <img src='../img/transparentFaces/transparent${genderOppositeKey}/3.82-WF-244-Trans.png'  class = 'partnerR'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 0,
             "right": 1
         }
-    ],[
+    ],
+    [
         {
             "pair": `<div class='bgImgCenter'>
               <img src='../img/transparentFaces/transparent${genderKey}/1.89-WM-228-Trans.png'  class = 'picpair' > 
               <img src='../img/transparentFaces/transparent${genderKey}/2.04-WM-236-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/4.76-WF-220-Trans.png'  class = 'partnerL'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 1,
@@ -87,20 +96,23 @@ const retifiedPairs = [
         },
         {
             "pair": `<div class='bgImgCenter'>
-              <img src='../img/transparentFaces/transparent${genderKey}/4.66-WM-004-Trans.png'  class = 'picpair' > 
-              <img src='../img/transparentFaces/transparent${genderKey}/4.59-WM-029-Trans.png'  class = 'picpair' ></img>
+              <img src='../img/transparentFaces/transparent${genderKey}/1.89-WM-228-Trans.png'  class = 'picpair' > 
+              <img src='../img/transparentFaces/transparent${genderKey}/2.04-WM-236-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/2.33-WF-239-Trans.png'  class = 'partnerR'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 0,
             "right": 1
         }
-    ],[
+    ],
+    [
         {
             "pair": `<div class='bgImgCenter'>
               <img src='../img/transparentFaces/transparent${genderKey}/2.6-WM-251-Trans.png'  class = 'picpair' > 
               <img src='../img/transparentFaces/transparent${genderKey}/2.12-WM-233-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/4.89-WF-003-Trans.png'  class = 'partnerL'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 1,
@@ -108,9 +120,10 @@ const retifiedPairs = [
         },
         {
             "pair": `<div class='bgImgCenter'>
-              <img src='../img/transparentFaces/transparent${genderKey}/4.66-WM-004-Trans.png'  class = 'picpair' > 
-              <img src='../img/transparentFaces/transparent${genderKey}/4.59-WM-029-Trans.png'  class = 'picpair' ></img>
+              <img src='../img/transparentFaces/transparent${genderKey}/2.6-WM-251-Trans.png'  class = 'picpair' > 
+              <img src='../img/transparentFaces/transparent${genderKey}/2.12-WM-233-Trans.png'  class = 'picpair' ></img>
               <img src='../img/transparentFaces/transparent${genderOppositeKey}/2.33-WF-239-Trans.png'  class = 'partnerR'>  </img> 
+              <div class ='whiterec'></div>
               </div>`,
             "id": 1,
             "left": 0,
